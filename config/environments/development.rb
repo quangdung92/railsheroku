f56f9c02afbe1d2b0b96f85b66bd9803238bff45
quangdung92 <quangdung.hvbc@gmail.com>
2015-10-22 13:59:27 +0700

    mailfix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,15 +19,15 @@ Rails.application.configure do
   config.action_mailer.default_options = {from: 'no-reply@grooovyware.jp'}
   # mail server setting
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: 'mail.grooovyware.jp',
-      port: 25,
-      user_name: 'egc',
-      password: 'egcpass',
-      authentication: 'plain',
-      enable_starttls_auto: true,
-      openssl_verify_mode: 'none'
-  }
+  # config.action_mailer.smtp_settings = {
+  #     address: 'mail.grooovyware.jp',
+  #     port: 25,
+  #     user_name: 'egc',
+  #     password: 'egcpass',
+  #     authentication: 'plain',
+  #     enable_starttls_auto: true,
+  #     openssl_verify_mode: 'none'
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
